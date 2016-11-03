42846890b24309f44dcebfdc43c7eac07e83cf02
Richa Patel <richa52trivedi@gmail.com>
2016-11-03 12:56:42 +0530

    Update Main.js

--- Ex6Buffer/Main.js
+++ Ex6Buffer/Main.js
@@ -1,1 +1,69 @@
 
+var buf1 = new Buffer(256);
+var len = buf1.write("Simply Easy Learning");
+
+console.log("Writing Buffer: Octets written = "+  len);
+
+
+var buf = new Buffer(26);
+for (var i = 0 ; i < 26 ; i++) {
+  buf[i] = i + 97;
+}
+console.log("Reading Buffer:");
+console.log( buf.toString('ascii'));       // outputs: abcdefghijklmnopqrstuvwxyz
+console.log( buf.toString('ascii',0,5));   // outputs: abcde
+console.log( buf.toString('utf8',0,5));    // outputs: abcde
+console.log( buf.toString(undefined,0,5)); // encoding defaults to 'utf8', outputs abcde
+
+
+console.log("Convert Buffer to JSON:");
+var buf2 = new Buffer('Simply Easy Learning');
+var json = buf2.toJSON(buf2);
+
+console.log(json);
+
+
+console.log("Concatenate Buffers:");
+var buffer1 = new Buffer('TutorialsPoint ');
+var buffer2 = new Buffer('Simply Easy Learning');
+var buffer3 = Buffer.concat([buffer1,buffer2]);
+console.log("buffer3 content: " + buffer3.toString());
+
+
+
+console.log("Compare Buffers:");
+
+var buffer1 = new Buffer('ABC');
+var buffer2 = new Buffer('ABCD');
+var result = buffer1.compare(buffer2);
+
+if(result < 0) {
+   console.log(buffer1 +" comes before " + buffer2);
+}else if(result == 0){
+   console.log(buffer1 +" is same as " + buffer2);
+}else {
+   console.log(buffer1 +" comes after " + buffer2);
+}
+
+
+console.log("Copy Buffers:");
+var buffer1 = new Buffer('ABC');
+
+//copy a buffer
+var buffer2 = new Buffer(3);
+buffer1.copy(buffer2);
+console.log("buffer2 content: " + buffer2.toString());
+
+console.log("Slice Buffers:");
+var buffer1 = new Buffer('TutorialsPoint');
+
+//slicing a buffer
+var buffer2 = buffer1.slice(0,9);
+console.log("buffer2 content: " + buffer2.toString());
+
+
+console.log("Length Buffers:");
+var buffer = new Buffer('TutorialsPoint');
+
+//length of the buffer
+console.log("buffer length: " + buffer.length);
