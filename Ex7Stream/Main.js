510e01a199c54da58e1a5a4c50805f2fdadb5380
Richa Patel <richa52trivedi@gmail.com>
2016-11-03 12:58:46 +0530

    Update Main.js

--- Ex7Stream/Main.js
+++ Ex7Stream/Main.js
@@ -1,1 +1,50 @@
 
+console.log("Reading from a Stream");
+
+var fs = require("fs");
+var data = '';
+
+// Create a readable stream
+var readerStream = fs.createReadStream('index.txt');
+
+// Set the encoding to be utf8. 
+readerStream.setEncoding('UTF8');
+
+// Handle stream events --> data, end, and error
+readerStream.on('data', function(chunk) {
+   data += chunk;
+});
+
+readerStream.on('end',function(){
+   console.log(data);
+});
+
+readerStream.on('error', function(err){
+   console.log(err.stack);
+});
+
+console.log("Reading Program Ended");
+
+console.log("Reading from a Stream");
+var fs = require("fs");
+var data = 'Simply Easy Learning';
+
+// Create a writable stream
+var writerStream = fs.createWriteStream('output.txt');
+
+// Write the data to stream with encoding to be utf8
+writerStream.write(data,'UTF8');
+
+// Mark the end of file
+writerStream.end();
+
+// Handle stream events --> finish, and error
+writerStream.on('finish', function() {
+    console.log("Write completed.");
+});
+
+writerStream.on('error', function(err){
+   console.log(err.stack);
+});
+
+console.log("Writeing Program Ended");
